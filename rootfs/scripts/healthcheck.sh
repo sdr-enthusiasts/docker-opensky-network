09991dc8de7e5650a2281b6b7ef7a3d02cac2695
Stanley F <stanley.foerster@gmail.com>
2024-05-17 07:29:59 +0200

    Fix issue in healthcheck.sh for IPv6 addresses (#56)
    * Fix issue in healthcheck.sh for IPv6 addresses
    
    In my case, the `getent hosts` command returns an IPv6 address but `netstat -anp` gives IPv4 addresses, so the last check fails. With this change, the IPv4 address gets resolved correctly.
    
    * explicitly call for IPv4 when using getent

--- rootfs/scripts/healthcheck.sh
+++ rootfs/scripts/healthcheck.sh
@@ -59,7 +59,7 @@ else
 fi
 
 # Attempt to resolve BEASTHOST into an IP address
-if BEASTIP=$(getent hosts "$BEASTHOST" 2> /dev/null | cut -d ' ' -f 1); then
+if BEASTIP=$(getent ahostsv4 "$BEASTHOST" | grep "$BEASTHOST" 2> /dev/null | cut -d ' ' -f 1); then
   :
   #echo "got host via getent"
 elif BEASTIP=$(s6-dnsip4 "$BEASTHOST" 2> /dev/null); then
