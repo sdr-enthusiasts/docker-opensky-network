fcaec7e4df76adaad675ae88cf2f61b4611e4b7c
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-12-18 11:32:13 -0700

    Infra Work
    chore: remove legacy pre-commit config
    Infra work

--- rootfs/etc/s6-overlay/scripts/01-opensky-network.sh
+++ rootfs/etc/s6-overlay/scripts/01-opensky-network.sh
@@ -4,67 +4,65 @@
 # Check to make sure the correct command line arguments have been set
 EXITCODE=0
 if [ -z "${LAT}" ]; then
-  echo "ERROR: LAT environment variable not set"
-  EXITCODE=1
+    echo "ERROR: LAT environment variable not set"
+    EXITCODE=1
 fi
 if [ -z "${LONG}" ]; then
-  echo "ERROR: LONG environment variable not set"
-  EXITCODE=1
+    echo "ERROR: LONG environment variable not set"
+    EXITCODE=1
 fi
 if [ -z "${BEASTHOST}" ]; then
-  echo "ERROR: BEASTHOST environment variable not set"
-  EXITCODE=1
+    echo "ERROR: BEASTHOST environment variable not set"
+    EXITCODE=1
 fi
 if [ -z "${ALT}" ]; then
-  echo "ERROR: ALT environment variable not set"
-  EXITCODE=1
+    echo "ERROR: ALT environment variable not set"
+    EXITCODE=1
 fi
 if [ -z "${OPENSKY_USERNAME}" ]; then
-  echo "ERROR: OPENSKY_USERNAME environment variable not set"
-  EXITCODE=1
+    echo "ERROR: OPENSKY_USERNAME environment variable not set"
+    EXITCODE=1
 fi
 if [ $EXITCODE -ne 0 ]; then
-  exit 1
+    exit 1
 fi
 
 # Set up timezone
 if [ -z "${TZ}" ]; then
-  echo "WARNING: TZ environment variable not set"
+    echo "WARNING: TZ environment variable not set"
 else
-  ln -snf "/usr/share/zoneinfo/$TZ" /etc/localtime && echo "$TZ" >/etc/timezone
+    ln -snf "/usr/share/zoneinfo/$TZ" /etc/localtime && echo "$TZ" >/etc/timezone
 fi
 
 # Generate config based on environment variables
 CONFIGFILE=/var/lib/openskyd/conf.d/10-opensky.conf
 {
-  echo "[GPS]"
-  echo "Latitude=${LAT}"
-  echo "Longitude=${LONG}"
-  echo "Altitude=${ALT}"
-  echo ""
-  echo "[DEVICE]"
-  echo "Type=${OPENSKY_DEVICE_TYPE}"
-  echo ""
-  echo "[IDENT]"
-  echo "Username=${OPENSKY_USERNAME}"
-  echo ""
-  echo "[INPUT]"
-  echo "Host=${BEASTHOST}"
-  # shellcheck disable=SC2153
-  echo "Port=${BEASTPORT}"
-} > ${CONFIGFILE}
+    echo "[GPS]"
+    echo "Latitude=${LAT}"
+    echo "Longitude=${LONG}"
+    echo "Altitude=${ALT}"
+    echo ""
+    echo "[DEVICE]"
+    echo "Type=${OPENSKY_DEVICE_TYPE}"
+    echo ""
+    echo "[IDENT]"
+    echo "Username=${OPENSKY_USERNAME}"
+    echo ""
+    echo "[INPUT]"
+    echo "Host=${BEASTHOST}"
+    echo "Port=${BEASTPORT:=30005}"
+} >${CONFIGFILE}
 
-if [ -z "${OPENSKY_SERIAL}" ]
-then
-  echo ""
-  echo "WARNING: OPENSKY_SERIAL environment variable was not set!"
-  echo "Please make sure you note down the serial generated."
-  echo "Pass the key as environment var OPENSKY_SERIAL on next launch!"
-  echo ""
+if [ -z "${OPENSKY_SERIAL}" ]; then
+    echo ""
+    echo "WARNING: OPENSKY_SERIAL environment variable was not set!"
+    echo "Please make sure you note down the serial generated."
+    echo "Pass the key as environment var OPENSKY_SERIAL on next launch!"
+    echo ""
 else
-  CONFIGFILE=/var/lib/openskyd/conf.d/05-serial.conf
-  {
-    echo "[Device]"
-    echo "serial = ${OPENSKY_SERIAL}"
-  } > ${CONFIGFILE}
+    CONFIGFILE=/var/lib/openskyd/conf.d/05-serial.conf
+    {
+        echo "[Device]"
+        echo "serial = ${OPENSKY_SERIAL}"
+    } >${CONFIGFILE}
 fi
